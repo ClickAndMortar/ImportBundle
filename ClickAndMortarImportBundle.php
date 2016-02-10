6436766d89a9d4cb1320b41f0e7a0a3c97c068a5
Simon CARRE <simoncarre54@gmail.com>
2016-02-10 23:04:16 +0100

    Init import bundle

---
+++ ClickAndMortarImportBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ClickAndMortar\ImportBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class ClickAndMortarImportBundle
+ *
+ * @package ClickAndMortar\ImportBundle
+ */
+class ClickAndMortarImportBundle extends Bundle
+{
+}
